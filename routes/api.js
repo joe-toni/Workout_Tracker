1a989e678cd8e6fb4ff8e733bd8219427f5b4cf8
joe-toni <joefaburrieta@gmail.com>
2021-12-23 00:52:08 -0800

    added the api route that puts a new exrecise in the latest workout based on id

--- routes/api.js
+++ routes/api.js
@@ -26,4 +26,16 @@ router.get("/workouts/range", async (req, res) => {
         res.status(400).json(err);
     }
     });
+
+    router.put("/workouts/:id", async (req, res) => {
+        try
+        {
+            const result = await db.Workout.findByIdAndUpdate(req.params.id, {$push:{exercises: req.body}});
+            res.status(200).json(result);
+        }
+        catch
+        {
+            res.status(400).json(err);
+        }
+        });
 module.exports = router;
