7a761389318fe7387823680b986787bce1096bd1
joe-toni <joefaburrieta@gmail.com>
2021-12-23 00:41:41 -0800

    got the stats page to display the latest workouts in the correct order

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const db = require("../models/workout");
 router.get("/workouts", async (req, res) => {
 try
 {
-    const result = await db.Workout.find({}).sort({date: -1});
+    const result = await db.Workout.find({}).sort({day : 1});
 
     res.status(200).json(result);
 }
@@ -12,6 +12,18 @@ catch
 {
     res.status(400).json(err);
 }
-})
+});
 
+router.get("/workouts/range", async (req, res) => {
+    try
+    {
+        const result = await db.Workout.find({}).sort({day: -1}).limit(7);
+        result.reverse();
+        res.status(200).json(result);
+    }
+    catch
+    {
+        res.status(400).json(err);
+    }
+    });
 module.exports = router;
